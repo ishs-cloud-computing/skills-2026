a8774a27a461d512f4898b9f9e4c3b04eb61fbef
zenru <zenru1023@gmail.com>
2026-07-10 13:56:04 +0900

    Remove ECR repo prefix and Add permission arg to Dockerfile

--- task-3/terraform/ecr.tf
+++ task-3/terraform/ecr.tf
@@ -1,7 +1,7 @@
 resource "aws_ecr_repository" "app" {
   for_each = local.apps
 
-  name         = "skills-${each.key}"
+  name         = each.key
   force_delete = true
 
   image_scanning_configuration {
